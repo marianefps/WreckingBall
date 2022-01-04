81ca74e55586a785092ff13e3e7dcf7ba96487c0
Mariane Fumagalli Prado e Sousa <68484075+marianefps@users.noreply.github.com>
2022-01-04 16:43:37 -0300

    Add files via upload

---
+++ WreckingBall.js
@@ -0,0 +1,24 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+//variaveis
+
+function setup() {
+  createCanvas(3000, 800);
+  engine = Engine.create();
+  world = engine.world;
+
+
+}
+
+function draw() {
+  background(180);
+  Engine.update(engine);
+  
+
+}
+
+
+
